cdec55f3c15b8dd7827eaa7e8fbde976d21318cc
Oyinda David <oyetunmibidavid9@gmail.com>
2019-02-23 05:04:48 +0000

    babel config and carousel module

--- App.js
+++ App.js
@@ -1,68 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
-import pf from "petfinder-client";
-
-
-const petfinder = pf({
-    key: process.env.API_KEY,
-    secret: process.env.API_SECRET
-});
-
-console.log(petfinder);
+import Results from "./Results";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            pets: []
-        };
-    }
-    componentDidMount() {
-        petfinder.pet
-            .find({ location: "Seattle, WA", output: "full" })
-            .then(data => {
-                let pets;
-                if (data.petfinder.pets && data.petfinder.pets.pet) {
-                    if (Array.isArray(data.petfinder.pets.pet)) {
-                        pets = data.petfinder.pets.pet;
-                    } else {
-                        pets = [data.petfinder.pets.pet];
-                    }
-                } else {
-                    pets = []
-                }
-                this.setState({
-                    pets
-                });
-            });
-    }
     render() {
         return (
-          <div>
-            <h1>Adopt Me!</h1>
-            {this.state.pets.map(pet => {
-              let breed;
-              if (Array.isArray(pet.breeds.breed)) {
-                breed = pet.breeds.breed.join(", ");
-              } else {
-                breed = pet.breeds.breed;
-              }
-              return (
-                <Pet
-                  animal={pet.animal}
-                  key={pet.id}
-                  name={pet.name}
-                  breed={breed}
-                  media={pet.media}
-                  location={`${pet.contact.city}, ${pet.contact.state}`}
-                />
-              );
-            })}
-          </div>
-        );
-      }
+            <div>
+             <header>
+             <Link to="/">Adopt Me!</Link>
+           </header>
+              <Router>
+                <Results path="/" />
+                <Details path="/details/:id" />
+              </Router>
+            </div>
+          );
+    }
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
